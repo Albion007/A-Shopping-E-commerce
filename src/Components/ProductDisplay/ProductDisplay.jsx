559d4795dab36d2705eb5690889a3d18ccb5ec49
am224ut <94832146+Albion007@users.noreply.github.com>
2024-02-26 14:57:28 +0100

    updated

--- src/Components/ProductDisplay/ProductDisplay.jsx
+++ src/Components/ProductDisplay/ProductDisplay.jsx
@@ -28,6 +28,27 @@ const ProductDisplay = (props) => {
             <img src={star_dull_icon} alt='' />
             <p>(122)</p>
         </div>
+        <div className="productdisplay-right-prices">
+          <div className="productdisplay-right-price-old">${product.old_price}</div>
+          <div className="productdisplay-right-price-new">${product.new_price}</div>
+        </div>
+        <div className="productdisplay-right-description">
+          Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+          Lorem Ipsum has been the industry's standard dummy text ever since the 1500s
+        </div>
+        <div className="productdisplay-right-size">
+          <h1>Select Size</h1>
+          <div className="productdisplay-right-size">
+            <div>S</div>
+            <div>M</div>
+            <div>L</div>
+            <div>XL</div>
+            <div>XXL</div>
+          </div>
+        </div>
+        <button>Add to cart</button>
+        <p className='productdisplay-right-category'><span>Category:</span>Women, T-Shirt, Crop Top</p>
+        <p className='productdisplay-right-category'><span>Tags:</span>Modern, Latest</p>
       </div>
     </div>
   )
