02552c95852d3f0fa99706adf9002854e6b521b5
am224ut <94832146+Albion007@users.noreply.github.com>
2024-02-28 14:36:51 +0100

    updated

--- src/Components/CartItems/CartItems.jsx
+++ src/Components/CartItems/CartItems.jsx
@@ -35,7 +35,31 @@ const CartItems = () => {
       })}
       <div className='cartitems-down'>
         <div className="cartitems-total">
-          
+          <h1>Cart Totals</h1>
+          <div>
+            <div className="cartitems-total-item">
+              <p>Subtotal</p>
+              <p>${0}</p>
+            </div>
+            <hr />
+            <div className='cartitems-total-item'>
+              <p>Shipping Fee</p>
+              <p>Free</p>
+            </div>
+            <hr />
+            <div className="cartitems-total-item">
+              <h3>Total</h3>
+              <h3>${0}</h3>
+            </div>
+          </div>
+          <button>PROCEED TO CHECKOUT</button>
+        </div>
+        <div className="cartitems-promocode">
+          <p>If you have a promo code, write it here</p>
+          <div className='cartitems-promobox'>
+            <input type='text' placeholder='promo code' />
+            <button>Submit</button>
+          </div>
         </div>
       </div>
     </div>
